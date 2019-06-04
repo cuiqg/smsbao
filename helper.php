8b8961d2980d912b448ee903f239fa6b9593db64
cuiqg <cuiqg@foxmail.com>
2019-06-04 14:59:01 +0800

    no message

--- helper.php
+++ helper.php
@@ -1,70 +1,57 @@
 <?php
 
-if(! function_exists('curlRequest')) {
+function curlRequest($url, $params = [], $method = 'get', $header = [], $timeout = 3) {
 
-    function curlRequest($url, $params = [], $method = 'get', $header = [], $timeout = 3) {
+    if(strcasecmp($method, 'get') == 0 && !empty($params)) {
 
-        if(strcasecmp($method, 'get') == 0 && !empty($params)) {
+        $url .= '?' . http_build_query($params);
+    }
 
-            $url .= '?' . http_build_query($params);
-        }
+    $ch = curl_init();
 
-        $ch = curl_init();
+    $opts = [
+        CURLOPT_URL            => $url,
+        CURLOPT_BINARYTRANSFER => TRUE,
+        CURLOPT_SSL_VERIFYPEER => FALSE,
+        CURLOPT_SSL_VERIFYHOST => FALSE,
+        CURLOPT_USERAGENT      => 'TsuiQG/1.0',
+        CURLOPT_TIMEOUT        => $timeout
+    ];
 
-        $opts = [
-            CURLOPT_URL            => $url,
-            CURLOPT_BINARYTRANSFER => TRUE,
-            CURLOPT_SSL_VERIFYPEER => FALSE,
-            CURLOPT_SSL_VERIFYHOST => FALSE,
-            CURLOPT_USERAGENT      => 'TsuiQG/1.0',
-            CURLOPT_TIMEOUT        => $timeout
+    if(strcasecmp($method, 'post') == 0) {
+        $opts[CURLOPT_POST]       = TRUE;
+        $opts[CURLOPT_POSTFIELDS] = $params;
+    }
+
+    if(!empty($header)) {
+        $opts[CURLOPT_HTTPHEADER] = $header;
+    }
+
+    curl_setopt_array($ch, $opts);
+    
+    $res = curl_exec($ch);
+    $error = curl_error($ch);
+    $errno = curl_errno($ch);
+    curl_close($ch);
+    
+    if($errno) {
+        return [
+            'error' => true,
+            'msg' => $error,
         ];
-
-        if(strcasecmp($method, 'post') == 0) {
-            $opts[CURLOPT_POST]       = TRUE;
-            $opts[CURLOPT_POSTFIELDS] = $params;
-        }
-
-        if(!empty($header)) {
-			$opts[CURLOPT_HTTPHEADER] = $header;
-        }
-
-        curl_setopt_array($ch, $opts);
-        
-        $res = curl_exec($ch);
-        $error = curl_error($ch);
-        $errno = curl_errno($ch);
-
-        if($errno) {
-            return [
-                'error' => true,
-                'msg' => $error,
-            ];
-        }
-
-        $res_dec = json_decode($res, true);
-
-        if( json_last_error()) {
-            return [
-                'error' => false,
-                'msg'   => '请求成功',
-                'res'   => $res,
-            ];
-        } else {
-            return [
-                'error' => false,
-                'msg'   => '请求成功',
-                'res'   => $res_dec,
-            ];
-        }
     }
 
-    function outText($text = '', $status = 200) {
-        header("Cache-Control: no-cache");
-        header("Pragma: no-cache");
-        header("Contetn-Type: text/html");
-        ob_start();
-        echo $text;
-        ob_end_flush();
-    }
+    return [
+        'error' => false,
+        'msg'   => '请求成功',
+        'res'   => $res,
+    ];
+}
+
+function outJson($data = []) {
+
+    header("X-AUTHOR-BY: TsuiQG");
+    header("Content-Type: application/json; charset=utf-8");
+    echo json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+    exit;
 }
--- index.php
+++ index.php
@@ -1,5 +1,94 @@
 <?php
 include_once __DIR__ . DIRECTORY_SEPARATOR . 'helper.php';
 
+$params = $_GET;
 
-outText('12121212');
+if(empty( $params)) {
+
+    outJson([
+        'code' => -1,
+        'message' => '缺少参数',
+        'data' => [
+            'params' => [
+                'u' => '平台的用户名',
+                'p' => '平台登录密码',
+                'm' => '接收的手机号;群发时多个手机号以逗号分隔，一次不要超过99个号码',
+                'c' => '发送内容'
+            ]
+        ]
+    ]);
+} else {
+
+    if(!isset($params['u']) || empty($params['u'])) {
+        outJson([
+            'code' => -1,
+            'message' => '缺少用户名',
+            'data' => [
+            ]
+        ]);
+    }
+
+    if(!isset($params['p']) || empty($params['p'])) {
+        outJson([
+            'code' => -1,
+            'message' => '缺少密码',
+            'data' => [
+            ]
+        ]);
+    }
+
+    if(!isset($params['m']) || empty($params['m'])) {
+        outJson([
+            'code' => -1,
+            'message' => '缺少手机号',
+            'data' => [
+            ]
+        ]);
+    }
+
+    if(!isset($params['c']) || empty($params['c'])) {
+        outJson([
+            'code' => -1,
+            'message' => '缺少短信内容',
+            'data' => [
+            ]
+        ]);
+    }
+
+    $params = [
+        'u' => $params['u'],
+        'p' => md5( $params['p']),
+        'm' => $params['m'],
+        'c' => $params['c'],
+    ];
+
+    $url = 'https://api.smsbao.com/sms'.'?'. http_build_query($params);
+    $res = file_get_contents($url);
+
+
+    $statusStr = array(
+        "0" => "短信发送成功",
+        "-1" => "参数不全",
+        "-2" => "服务器空间不支持,请确认支持curl或者fsocket，联系您的空间商解决或者更换空间！",
+        "30" => "密码错误",
+        "40" => "账号不存在",
+        "41" => "余额不足",
+        "42" => "帐户已过期",
+        "43" => "IP地址限制",
+        "50" => "内容含有敏感词"
+        );
+
+    if($res > 0) {
+        outJson([
+            'code'    => -1,
+            'message' => $statusStr[$res],
+            'data'    => []
+        ]);
+    } else {
+        outJson([
+            'code'    => 0,
+            'message' => $statusStr[$res],
+            'data'    => [],
+        ]);
+    }
+}
